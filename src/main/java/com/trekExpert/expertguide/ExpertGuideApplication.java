e4c2e3b6fbc98d545be518bc971d8784a7d85703
Dhanz21 <shajithdhanya15@gmail.com>
2026-07-21 11:04:47 +0100

    Add Swagger OpenAPI configuration to ExpertGuideApplication

--- src/main/java/com/trekExpert/expertguide/ExpertGuideApplication.java
+++ src/main/java/com/trekExpert/expertguide/ExpertGuideApplication.java
@@ -2,10 +2,23 @@ package com.trekExpert.expertguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExpertGuideApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ExpertGuideApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ExpertGuide API")
+                        .version("1.0.0")
+                        .description("Expert Guide REST API with Swagger Documentation"));
     }
 }
